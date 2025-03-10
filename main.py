06b019a4ef1c2096c518d4430cabb13e206356e2
suraci00 <simo.sura00@gmail.com>
2025-03-10 10:27:58 +0100

    Hiding sensitive files

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 from flask import Flask,request
 from google.cloud import pubsub_v1
+from google.cloud import firestore
 from json import loads
 from base64 import b64decode
 from secret import project_id,topic_name
 
 base_url = 'https://pcloudsmarthouse.ew.r.appspot.com/'
 app = Flask(__name__)
-
+'''db = 'smarthouse'
+coll = 'sensors'
+db = firestore.Client.from_service_account_json('credentials.json', database = db)'''
 @app.route('/',methods=['GET'])
 def main():
     return 'ok'
@@ -19,7 +22,19 @@ def pubsub_write():
     r = publisher.publish(topic_path, b'sensor', key1='')
     return r.result()
 
-@app.route('/pubsub/push',methods=['POST'])
+
+'''def add_data(s):
+    data = request.values['data']
+    val = float(request.values['val'])
+    doc_ref = db.collection(coll).document(s)
+    if doc_ref.get().exists:
+        l = doc_ref.get().to_dict()['sensors']
+        l.append(data, val)
+    else:
+        doc_ref.set({'sensors': [(data, val)]})
+    return 'OK', 200'''
+
+@app.route('pubsub/push', methods=['POST'])
 def pubsub_push():
     print('ricevuto payload',flush=True)
     dict = loads(request.data.decode('utf-8')) # deserializzazione
