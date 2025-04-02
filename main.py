e071050962062fb9ce767099a9fffebcd71da15a
suraci00 <simo.sura00@gmail.com>
2025-04-03 01:28:01 +0200

    fix

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def get_data(s):
 def graph(s):
     print(s)
     d = loads(get_data(s))
-    ds = "[\n" + ",\n".join(f"['{date}', {status}]" for date, status in d) + "\n]"
+    ds = "[\n" + ",\n".join(f"['{date}', '{status}']" for date, status in d) + "\n]"
     return render_template('graph.html', data = ds)
 
 @app.route('/map',methods=['GET'])
